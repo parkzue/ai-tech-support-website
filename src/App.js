cbcda9a2a1f94d9fc08c9b027ef69adce5626171
parkzue <110686167+parkzue@users.noreply.github.com>
2023-11-29 13:48:01 +0200

    rewrote page in React JS, initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import lpe6lpe8_cover from './images/lpe6lpe8.png';
+import lpe6flpe8f_cover from './images/lpe6flpe8f.png';
+import combigenius_cover from './images/combigenius.png';
 
-function App() {
+const App = () => {
+  const handleImageClick = (url) => {
+    window.location.href = url;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Placeholder Title</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div onClick={() => handleImageClick('https://example.com')}>
+          <img src={lpe6lpe8_cover} alt="Guide 1" style={{ cursor: 'pointer', maxWidth: '100%' }} />
+        </div>
+        <div onClick={() => handleImageClick('https://example.com')}>
+          <img src={lpe6flpe8f_cover} alt="Guide 2" style={{ cursor: 'pointer', maxWidth: '100%' }} />
+        </div>
+        <div onClick={() => handleImageClick('https://example.com')}>
+          <img src={combigenius_cover} alt="Guide 3" style={{ cursor: 'pointer', maxWidth: '100%' }} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
