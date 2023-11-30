420af78d0a65e6add73c467771fbc76de72f6ada
parkzue <110686167+parkzue@users.noreply.github.com>
2023-11-30 18:22:00 +0200

    Improved code structure

--- src/App.js
+++ src/App.js
@@ -2,79 +2,54 @@ import React, { useState } from 'react';
 import lpe6lpe8_cover from './images/lpe6lpe8.png';
 import lpe6flpe8f_cover from './images/lpe6flpe8f.png';
 import combigenius_cover from './images/combigenius.png';
-//import GuideCard from './components/GuideCard';
-//import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+import GuideCard from './components/GuideCard';
 
 const App = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedGuide, setSelectedGuide] = useState(null);
 
   const handleImageClick = (url) => {
     window.location.href = url;
   };
 
   const handleImageMouseEnter = (url, event) => {
-    setSelectedImage(url);
+    setSelectedGuide(url);
   };
 
   const handleImageMouseLeave = () => {
-    setSelectedImage(null);
-  };
-
-  const renderIcon = (url) => {
-    const guide = document.getElementById(url);
-    if (!guide) return null;
-
-    const rect = guide.getBoundingClientRect();
-    const iconStyle = {
-      position: 'absolute',
-      top: `${rect.top + rect.height / 2}px`,
-      left: `${rect.left + rect.width / 2}px`,
-      transform: 'translate(-50%, -50%)',
-      background: 'rgba(255, 255, 255, 0.9)',
-      padding: '5px',
-      borderRadius: '5px',
-      boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)',
-      zIndex: 1,
-    };
-
-    return (
-      <div style={iconStyle}>
-        <span>Chat with this guide</span>
-      </div>
-    );
+    setSelectedGuide(null);
   };
 
   return (
     <div>
-      <h1>User Guides</h1>
+      <h1>User</h1>
       <div style={{ display: 'flex', justifyContent: 'space-around', position: 'relative' }}>
-        <div
-          id="https://example.com"
-          onClick={() => handleImageClick('https://example.com')}
-          onMouseEnter={(event) => handleImageMouseEnter('https://example.com', event)}
+        <GuideCard
+          imageUrl={lpe6lpe8_cover}
+          guideUrl="https://example.com"
+          altText="lpe6lpe8"
+          onClick={handleImageClick}
+          onMouseEnter={handleImageMouseEnter}
           onMouseLeave={handleImageMouseLeave}
-        >
-          <img src={lpe6lpe8_cover} alt="Guide 1" style={{ cursor: 'pointer', maxWidth: '100%' }} />
-          {selectedImage === 'https://example.com' && renderIcon('https://example.com')}
-        </div>
-        <div
-          id="https://google.com"
-          onClick={() => handleImageClick('https://google.com')}
-          onMouseEnter={(event) => handleImageMouseEnter('https://google.com', event)}
+          selected={selectedGuide === 'https://example.com'}
+        />
+        <GuideCard
+          imageUrl={lpe6flpe8f_cover}
+          guideUrl="https://example.net"
+          altText="lpe6flpe8f"
+          onClick={handleImageClick}
+          onMouseEnter={handleImageMouseEnter}
           onMouseLeave={handleImageMouseLeave}
-        >
-          <img src={lpe6flpe8f_cover} alt="Guide 2" style={{ cursor: 'pointer', maxWidth: '100%' }} />
-          {selectedImage === 'https://google.com' && renderIcon('https://google.com')}
-        </div>
-        <div
-          id="https://github.com"
-          onClick={() => handleImageClick('https://github.com')}
-          onMouseEnter={(event) => handleImageMouseEnter('https://github.com', event)}
+          selected={selectedGuide === 'https://example.net'}
+        />
+        <GuideCard
+          imageUrl={combigenius_cover}
+          guideUrl="https://example.org"
+          altText="combigenius"
+          onClick={handleImageClick}
+          onMouseEnter={handleImageMouseEnter}
           onMouseLeave={handleImageMouseLeave}
-        >
-          <img src={combigenius_cover} alt="Guide 3" style={{ cursor: 'pointer', maxWidth: '100%' }} />
-          {selectedImage === 'https://github.com' && renderIcon('https://github.com')}
-        </div>
+          selected={selectedGuide === 'https://example.org'}
+        />
       </div>
     </div>
   );
--- src/components/GuideCard.js
+++ src/components/GuideCard.js
@@ -1,9 +1,10 @@
-// components/GuideCard.js
+
 import React from 'react';
 import './GuideCard.css';
 
 const GuideCard = ({ imageUrl, guideUrl, altText, onClick, onMouseEnter, onMouseLeave, selected }) => (
   <div
+    className={selected ? 'guide-card selected' : 'guide-card'}
     onClick={() => onClick(guideUrl)}
     onMouseEnter={(event) => onMouseEnter(guideUrl, event)}
     onMouseLeave={onMouseLeave}
@@ -11,11 +12,10 @@ const GuideCard = ({ imageUrl, guideUrl, altText, onClick, onMouseEnter, onMouse
     <img src={imageUrl} alt={altText} style={{ cursor: 'pointer', maxWidth: '100%' }} />
     {selected && (
       <div className="icon">
-        <span>Selected URL: {guideUrl}</span>
+        <span>Chat with this guide: {altText}</span>
       </div>
     )}
   </div>
 );
 
 export default GuideCard;
-
