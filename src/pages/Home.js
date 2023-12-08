4d67c076b6b0a288ae218dacf469cf38f0ddeb27
parkzue <110686167+parkzue@users.noreply.github.com>
2023-12-08 02:50:29 +0200

    fixed again using Navigate from react router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,14 @@ import lpe6lpe8_cover from '../images/lpe6lpe8.png';
 import lpe6flpe8f_cover from '../images/lpe6flpe8f.png';
 import combigenius_cover from '../images/combigenius.png';
 import GuideCard from '../components/GuideCard';
-import {useLocation} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
     const [selectedGuide, setSelectedGuide] = useState(null);
-    const location = useLocation();
-    var url_lpe6lpe8 = location.pathname + "lpe6lpe8";
-    var url_lpe6flpe8f = location.pathname + "lpe6flpe8f";
-    var url_combigenius = location.pathname + "combigenius";
+    const navigate = useNavigate();
 
     const handleImageClick = (url) => {
-      window.location.href = url;
+        navigate('/' + url);
     };
     
     const handleImageMouseEnter = (url, event) => {
@@ -30,30 +27,30 @@ const Home = () => {
     <div style={{ display: 'flex', justifyContent: 'space-around', position: 'relative' }}>
       <GuideCard
         imageUrl={lpe6lpe8_cover}
-        guideUrl={url_lpe6lpe8}
+        guideUrl="lpe6lpe8"
         altText="lpe6lpe8"
         onClick={handleImageClick}
         onMouseEnter={handleImageMouseEnter}
         onMouseLeave={handleImageMouseLeave}
-        selected={selectedGuide === url_lpe6lpe8}
+        selected={selectedGuide === "lpe6lpe8"}
       />
       <GuideCard
         imageUrl={lpe6flpe8f_cover}
-        guideUrl={url_lpe6flpe8f}
+        guideUrl="lpe6flpe8f"
         altText="lpe6flpe8f"
         onClick={handleImageClick}
         onMouseEnter={handleImageMouseEnter}
         onMouseLeave={handleImageMouseLeave}
-        selected={selectedGuide === url_lpe6flpe8f}
+        selected={selectedGuide === "lpe6flpe8f"}
       />
       <GuideCard
         imageUrl={combigenius_cover}
-        guideUrl={url_combigenius}
+        guideUrl="combigenius"
         altText="combigenius"
         onClick={handleImageClick}
         onMouseEnter={handleImageMouseEnter}
         onMouseLeave={handleImageMouseLeave}
-        selected={selectedGuide === url_combigenius}
+        selected={selectedGuide === "combigenius"}
       />
     </div>
   </div>
